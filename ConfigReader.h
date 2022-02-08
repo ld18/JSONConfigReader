bf1010d4b78a1a76fb61596d87a3348c0b91ac22
l d <l.dreher007@gmail.com>
2022-02-08 18:15:25 +0100

    switched to templates and removed the static srchitecture, reworked the locking and added support for arrays

--- ConfigReader.h
+++ ConfigReader.h
@@ -5,32 +5,84 @@
 #include <atomic>
 #include <shared_mutex>
 #include <vector>
+#include <type_traits>
 #include "json.h"
 
 #define regexExpression_KeySeperator "[^\/]+"
 
 using std::string;
+using std::stoi;
 using std::atomic;
 using std::shared_mutex;
+using std::shared_lock;
+using std::unique_lock;
 using std::vector;
+using json::parsing_error;
 
 class ConfigReader {
 public:
 	ConfigReader() = delete;
-	static bool loadConfogfile(const string& path);
-	static int get_int(const string& Keys);
-	static int get_int(const string& Keys, const int& defaultValue);
-	static string get_string(const string& Keys);
-	static string get_string(const string& Keys, const string& defaultValue);
-	static float get_float(const string& Keys);
-	static float get_float(const string& Keys, const float& defaultValue);
-	static bool get_bool(const string& Keys);
-	static bool get_bool(const string& Keys, const bool& defaultValue);
-private:
-	static void splitPath(const string& keyPath, vector<string>& keys);
-	static json::jobject jsonDoc;
-	static atomic<bool> dirty;
-	static shared_mutex mutex;
-};
+	ConfigReader(const string& path);
 
+	template < typename T, class = typename std::enable_if<std::is_arithmetic<T>::value || std::is_same<std::string, T>::value>::type>
+	T get(const string& Keys) const {
+		shared_lock<shared_mutex> sharedLock(mutex);
+		if (!dirty.load()) {
+			vector<string> subKeys;
+			splitPath(Keys, subKeys);
+			json::jobject obj = jsonDoc;
+			for (unsigned int i = 0; i < subKeys.size() - 1; i++) {
+				if (obj.is_array()) {
+					try {
+						obj = obj.array(stoi(subKeys[i]));
+					}
+					catch (...) {
+						throw new json::invalid_key("ConfigReader::get cant get array value, key: " + subKeys.back());
+					}
+				} else {
+					obj = obj[subKeys[i]].as_object();
+				}
+			}				
+			T value;
+			if (obj.is_array()) {
+				try	{
+					value = static_cast<T>(obj.array(stoi(subKeys.back())));
+				} catch (...) {
+					throw new json::invalid_key("ConfigReader::get cant get array value, key: " + subKeys.back());
+				}
+			}
+			else {
+				value = static_cast<T>(obj[subKeys.back()]);
+			}
+			return value;
+		}
+		else {
+			throw new parsing_error("ConfigReader::get cant open file because it is dirty");
+		}
+	}
+
+	template < typename T, class = typename std::enable_if<std::is_arithmetic<T>::value || std::is_same<std::string, T>::value>::type>
+	T get(const string& Keys, const T& defaultValue, bool& foundKey) const {
+		try {
+			foundKey = true;
+			return get<T>(Keys);
+		}
+		catch (const json::invalid_key) {
+			foundKey = false;
+			return defaultValue;
+		}
+	}
+
+	template < typename T, class = typename std::enable_if<std::is_arithmetic<T>::value || std::is_same<std::string, T>::value>::type>
+	T get(const string& Keys, const T& defaultValue) const {
+		bool found;
+		return get(Keys, defaultValue, found);
+	}
+
+private:
+	void splitPath(const string& keyPath, vector<string>& keys) const;
+	json::jobject jsonDoc;
+	atomic<bool> dirty;
+	mutable shared_mutex mutex;
+};
 #endif
